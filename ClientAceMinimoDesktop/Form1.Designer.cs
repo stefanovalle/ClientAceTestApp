fbc89990c9771e61236f8a12984218b6ca9a869d
mauro <mauro.rainis@gmail.com>
2018-01-12 19:33:06 +0100

    Aggiungere i file di progetto.

---
+++ ClientAceMinimoDesktop/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ClientAceMinimoDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.debugBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.realtime = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // debugBox
+            // 
+            this.debugBox.Location = new System.Drawing.Point(6, 38);
+            this.debugBox.Multiline = true;
+            this.debugBox.Name = "debugBox";
+            this.debugBox.Size = new System.Drawing.Size(709, 86);
+            this.debugBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Channel1.Device1.Tag2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(224, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(189, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Simulation Examples.Functions.Ramp1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Aggiorna";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // realtime
+            // 
+            this.realtime.Location = new System.Drawing.Point(6, 19);
+            this.realtime.Multiline = true;
+            this.realtime.Name = "realtime";
+            this.realtime.Size = new System.Drawing.Size(425, 159);
+            this.realtime.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(278, 211);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lettura a richiesta";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.realtime);
+            this.groupBox2.Location = new System.Drawing.Point(296, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(443, 211);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Lettura in real time";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.debugBox);
+            this.groupBox3.Location = new System.Drawing.Point(12, 236);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(721, 141);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Debug Area";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 393);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox debugBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox realtime;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ ClientAceMinimoDesktop/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Kepware.ClientAce.OpcDaClient;
+
+namespace ClientAceMinimoDesktop
+{
+    public partial class Form1 : Form
+    {
+        DaServerMgt daServerMgt = new Kepware.ClientAce.OpcDaClient.DaServerMgt();
+        ConnectInfo connectInfo = new Kepware.ClientAce.OpcDaClient.ConnectInfo();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Event handler
+            daServerMgt.DataChanged += DaServerMgt_DataChanged;
+        }
+
+        private void DaServerMgt_DataChanged(int clientSubscription, bool allQualitiesGood, bool noErrors, ItemValueCallback[] itemValues)
+        {
+            try
+            {
+                foreach (ItemValueCallback itemValue in itemValues)
+                {
+                    if (itemValue.ResultID.Succeeded)
+                    {
+                        
+                        realtime.Text += itemValue.TimeStamp + "  " + itemValue.Value + Environment.NewLine;
+                        realtime.SelectionStart = realtime.TextLength;
+                        realtime.ScrollToCaret();
+                    }
+                    else
+                    {
+                        debugBox.Text += "Errore";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DataChanged exception. Reason: {0}", ex);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectInfo.LocalId = "en";
+            connectInfo.KeepAliveTime = 5000;
+            connectInfo.RetryAfterConnectionError = true;
+            connectInfo.RetryInitialConnection = true;
+            connectInfo.ClientName = "Demo ClientAceC-Sharp DesktopApplication";
+            bool connectFailed;
+            string url = "opcda://127.0.0.1/Kepware.KEPServerEX.V6/{7BC0CC8E-482C-47CA-ABDC-0FE7F9C6E729}";
+            int clientHandle = 1;
+
+            // Connessione al server
+            try
+            {
+                daServerMgt.Connect(url, clientHandle, ref connectInfo, out connectFailed);
+                if (connectFailed)
+                {
+                    debugBox.Text  = "Connect failed" + Environment.NewLine;
+                }
+                else
+                {
+                    debugBox.Text = "Connected to Server "+ url + " Succeeded." + Environment.NewLine;
+                }
+            }
+            catch (Exception ex)
+            {
+                debugBox.Text = ex.ToString();
+            }
+
+            AggiornaDati();
+            
+            // Tag a cui mi voglio sottoscrivere
+            ItemIdentifier[] itemIdentifiers = new ItemIdentifier[2];
+            itemIdentifiers[0] = new ItemIdentifier();
+            itemIdentifiers[0].ItemName = "Channel1.Device1.Tag2";
+            itemIdentifiers[0].ClientHandle = 1; // Assign uniquehandle
+
+            itemIdentifiers[1] = new ItemIdentifier();
+            itemIdentifiers[1].ItemName = "Simulation Examples.Functions.Ramp1";
+            itemIdentifiers[1].ClientHandle = 2; // Assign uniquehandle
+            int serverSubscription;
+            ReturnCode returnCode;
+            
+            // Parametri di sottoscrizione agli eventi
+            int clientSubscription = 1;
+            bool active = true;
+            int updateRate = 1000;
+            int revisedUpdateRate;
+            float deadband = 0;
+
+            try
+            {
+                // Sottoscrizione agli eventi change data
+                returnCode = daServerMgt.Subscribe(clientSubscription,
+                active,
+                updateRate,
+                out revisedUpdateRate,
+                deadband,
+                ref itemIdentifiers,
+                out serverSubscription);
+            }
+            catch (Exception ex)
+            {
+                debugBox.Text = ex.ToString();
+            }
+            
+        }
+
+        private void AggiornaDati()
+        {
+            // Aggiorno a mano i valori di due tag
+
+            int maxAge = 0;
+            Kepware.ClientAce.OpcDaClient.ItemIdentifier[] OPCItems = new Kepware.ClientAce.OpcDaClient.ItemIdentifier[2];
+            Kepware.ClientAce.OpcDaClient.ItemValue[] OPCItemValues = null;
+
+            OPCItems[0] = new Kepware.ClientAce.OpcDaClient.ItemIdentifier();
+            OPCItems[0].ItemName = "Channel1.Device1.Tag2";
+            OPCItems[0].ClientHandle = 1;
+
+            OPCItems[1] = new Kepware.ClientAce.OpcDaClient.ItemIdentifier();
+            OPCItems[1].ItemName = "Simulation Examples.Functions.Ramp1";
+            OPCItems[1].ClientHandle = 2;
+
+            label3.Text = OPCItems[0].ItemName;
+            label6.Text = OPCItems[1].ItemName;
+
+            try
+            {
+                daServerMgt.Read(maxAge, ref OPCItems, out OPCItemValues);
+
+                if (OPCItems[0].ResultID.Succeeded & OPCItemValues[0].Quality.IsGood)
+                {
+                    label4.Text = OPCItemValues[0].Value.ToString();
+                }
+                else
+                {
+                    debugBox.Text += OPCItems[0].ResultID.Description;
+                }
+
+                if (OPCItems[1].ResultID.Succeeded & OPCItemValues[1].Quality.IsGood)
+                {
+                    label5.Text = OPCItemValues[1].Value.ToString();
+                }
+                else
+                {
+                    debugBox.Text += OPCItems[1].ResultID.Description;
+                }
+            }
+            catch (Exception ex)
+            {
+                debugBox.Text += ex.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AggiornaDati();
+        }
+    }
+}
